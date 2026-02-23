1f24942ce88748d75a5a1111befe3bc3ad17f44a
agri-student <143807599+agri-student@users.noreply.github.com>
2026-02-23 16:29:32 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDLl4vT2IjCZKOrqtzfMEmBLRQ81ETUht8",
-  authDomain: "seiiku2.firebaseapp.com",
-  projectId: "seiiku2",
-  storageBucket: "seiiku2.firebasestorage.app",
-  messagingSenderId: "382357095429",
-  appId: "1:382357095429:web:6a8db8deb7a8febecbc86c",
-  measurementId: "G-KVFV3MH3T3"
-};
+
